ba67caa40392b4815cd964ca66b0ce28c19cca61
Kyle <kylevhuang@gmail.com>
2022-02-03 00:47:46 -0800

    fixed join button

--- frontend/src/components/JoinButton/index.js
+++ frontend/src/components/JoinButton/index.js
@@ -6,20 +6,19 @@ import Joined from "./joined";
 import Unjoined from "./unjoined";
 function JoinButton({ cp, sessionId }) {
   const isFollowing = useSelector((state) => state.isFollowing);
-  const currentPost = useSelector((state) => state.currentPost);
   const dispatch = useDispatch();
   async function loadData() {
-    if (cp && sessionId) await dispatch(checkFollow(cp, sessionId));
+    if (cp) await dispatch(checkFollow(cp, sessionId));
   }
   useEffect(() => {
     loadData();
-  }, [cp]);
+  }, [cp, sessionId]);
   return (
     <>
       {isFollowing ? (
-        <Joined cp={currentPost.page?.id} sessionId={sessionId} />
+        <Joined cp={cp} sessionId={sessionId} />
       ) : (
-        <Unjoined cp={currentPost.page?.id} sessionId={sessionId} />
+        <Unjoined cp={cp} sessionId={sessionId} />
       )}
     </>
   );
