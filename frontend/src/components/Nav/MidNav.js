946a288470ddd1958afebb722b148f305aee62a3
Kyle <kylevhuang@gmail.com>
2022-02-10 09:53:09 -0800

    search bar frontend

--- frontend/src/components/Nav/MidNav.js
+++ frontend/src/components/Nav/MidNav.js
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react";
+
 function MidNav() {
+  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState(false);
+  useEffect(() => {
+    function close() {
+      setSearch(false);
+    }
+    document.body.addEventListener("click", close);
+    return () => {
+      document.body.removeEventListener("click", close);
+    };
+  });
   return (
     <div className="midNav">
-      <input placeholder="Search for Communities"></input>
-      <div className="dropResult"></div>
+      <input
+        placeholder="Search for Communities"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        onClick={(e) => {
+          e.stopPropagation();
+          setSearch(true);
+        }}
+      ></input>
+      {search && <div className="dropResult"></div>}
     </div>
   );
 }
